79e0e3249434b663a384c3640bd065f93e40df00
Momen Ahmed Abdelhay <117471310+Momen-Ahmed74@users.noreply.github.com>
2023-04-19 06:10:21 +0200

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const studentsFunction = (request, respose) => {
                  "<ul> <li> <h2> Momen Ahmed Abdelhay </h2> </li> </ul>" +
                  "<ul> <li> <h2> Ahmed Mohammed Ali </h2> </li> </ul>" +
                  "<ul> <li> <h2> Karim Mohammed Ahmed </h2> </li> </ul>" +
-                 "<ul> <li> <h2> Yasser Adelrahman Mohammed </h2> </li> </ul>" +
+                 "<ul> <li> <h2> Yasser Adelrahman Tamer </h2> </li> </ul>" +
                  "<ul> <li> <h2> Mohmmed Ali Ahmed </h2> </li> </ul>")
 };
 
